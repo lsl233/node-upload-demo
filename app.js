2078f5ace85e38de530fc5011aef9039e2757585
李石磊 <1156021653@qq.com>
2018-05-10 10:14:57 +0800

    调整

--- app.js
+++ app.js
@@ -13,21 +13,25 @@ app.use(async ctx => {
     if (ctx.originalUrl === '/api/upload') {
         const {req} = ctx;
         const form = new multiparty.Form();
-        form.encoding = 'utf-8'; // 编码
-        form.keepExtensions = true; // 保留扩展名
-        // form.maxFieldsSize = 2 * 1024 * 1024; // 文件大小
-        // form.uploadDir='tmp';
-        form.parse(req, function (err, fields, files) {
+        form.encoding = 'utf-8';
+        form.keepExtensions = true;
+        form.parse(req, (err, fields, files) => {
             const fileArr = files.files;
             for (const file of fileArr) {
                 const uploadedPath = file.path;
-                const dstPath = './' + file.originalFilename;
+                const dstPath = './download/' + file.originalFilename;
                 const readStream  = fs.createReadStream(uploadedPath);
                 const writeStream = fs.createWriteStream(dstPath);
-                readStream.pipe(writeStream)
+                readStream.pipe(writeStream);
+                readStream.on('end', () => {
+                    console.log('readStream end')
+                })
             }
+            ctx.body = 'success';
         });
     }
 });
 
 app.listen(3000);
+
+console.info('node-upload-demo start, port:', 3000);
