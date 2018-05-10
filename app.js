31552674e446b93a83e39118529558f6711ff367
李石磊 <1156021653@qq.com>
2018-05-10 11:45:41 +0800

    调整

--- app.js
+++ app.js
@@ -2,33 +2,46 @@ const Koa = require('koa');
 const koaStatic = require('koa-static');
 const multiparty = require('multiparty');
 const fs = require('fs');
-const util = require('util');
 const app = new Koa();
+
+app.use(koaStatic('download'));
 
 app.use(koaStatic('views'), {
     defer: true
 });
 
-app.use(async ctx => {
-    if (ctx.originalUrl === '/api/upload') {
-        const {req} = ctx;
+function readFormData (req) {
+    return new Promise((resolve, reject) => {
         const form = new multiparty.Form();
         form.encoding = 'utf-8';
         form.keepExtensions = true;
         form.parse(req, (err, fields, files) => {
-            const fileArr = files.files;
-            for (const file of fileArr) {
-                const uploadedPath = file.path;
-                const dstPath = './download/' + file.originalFilename;
-                const readStream  = fs.createReadStream(uploadedPath);
-                const writeStream = fs.createWriteStream(dstPath);
-                readStream.pipe(writeStream);
-                readStream.on('end', () => {
-                    console.log('readStream end')
-                })
+            if (err) {
+                reject(err);
+            } else {
+                resolve({fields, files});
             }
-            ctx.body = 'success';
-        });
+        })
+    })
+}
+
+app.use(async ctx => {
+    if (ctx.originalUrl === '/api/upload') {
+        const {req} = ctx;
+        const {fields, files} = await readFormData(req);
+        const fileArr = files.files;
+        console.log(fields);
+        for (const file of fileArr) {
+            const uploadedPath = file.path;
+            const dstPath = './download/' + file.originalFilename;
+            const readStream  = fs.createReadStream(uploadedPath);
+            const writeStream = fs.createWriteStream(dstPath);
+            readStream.pipe(writeStream);
+            readStream.on('end', () => {
+                console.log('readStream end')
+            });
+        }
+        ctx.body = {status: 'done'};
     }
 });
 
